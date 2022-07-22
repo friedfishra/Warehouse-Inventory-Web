6d9e6ae5d32dd2fa37fb1d1e61462a2cde62b429
Atul Mishra <atulabhishek3@gmail.com>
2022-07-21 21:22:19 -0400

    added more styles to add-item and changed text to dropdown

--- warehouse-app/src/app/add-item/add-item.component.ts
+++ warehouse-app/src/app/add-item/add-item.component.ts
@@ -10,12 +10,18 @@ import { Item } from '../models/Item';
 export class AddItemComponent implements OnInit {
   itemFormData: Item
   weights :number[];
+  weightTypes :string[];
+  zoneIds: number[];
+  aisles: number[];
   selectedWeight!: number;
 
   service: ItemApiService
   constructor(service: ItemApiService) { 
     this.itemFormData = new Item()
     this.weights = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+    this.weightTypes = ["Dumbbell", "Kettlebell", "Bar", "Plates", "Medicine Ball"]
+    this.zoneIds = [1,2,3]
+    this.aisles = [1, 2, 3, 4, 5, 6, 7, 8]
 
     this.service = service
   }
@@ -28,5 +34,5 @@ export class AddItemComponent implements OnInit {
     
   }
 
-  
+
 }
